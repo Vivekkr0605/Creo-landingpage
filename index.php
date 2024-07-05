12572506f8e7e8eb38b1f97fa5dd20d9e1448bc4
Vivekkr0605 <vivekkumar45622@gmail.com>
2024-07-05 15:49:12 +0530

    creo

--- index.php
+++ index.php
@@ -9,7 +9,47 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+   
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/fontawesome.min.css" integrity="sha512-UuQ/zJlbMVAw/UU8vVBhnI4op+/tFOpQZVT+FormmIEhRSCnJWyHiBbEVgM4Uztsht41f3FzVWgLuwzUqOObKw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+   <style>
+    html,body
+{
+    width: 100%;
+    height: 100%;
+    margin: 0px;
+    padding: 0px;
+    overflow-x: hidden; 
+}
+
+
+@media (max-width: 576px) { 
+     h2, h3, h5{
+        text-align: center;
+    }
+    .topimg{
+        flex-direction: column;
+        width: 80%
+    }
+    .topimg img {
+        width: 100%;
+    }
+    #img2{
+        width: 80%;
+        height: auto;
+    }
+    .section1{
+        flex-direction: column;
+        width: 80%
+    }
+    .section1-a{
+        width: max-content;
+        padding-left: 40px;
+        justify-content: space-evenly;
+        
+    }
+
+ }
+   </style>
 </head>
 
 <body>
@@ -18,7 +58,7 @@
             <a class="nav-link active text-black --bs-nav-link-hover-color:blue;" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link text-black" href="#">Home</a>
+            <a class="nav-link abc text-black" href="#">Home</a>
         </li>
         <li class="nav-item">
             <a class="nav-link text-black" href="#">Home</a>
@@ -33,9 +73,9 @@
 
     <div class="container-fluid">
         <div class="row bg-dark pt-3">
-            <div class="col-12 d-flex justify-content-around">
+            <div class="col-12 d-flex justify-content-around section1">
                 <div class="float-start align-self-center">
-                    <ul class="d-flex list-unstyled">
+                    <ul class="d-flex list-unstyled justify-content-evenly section1-a">
                         <li class="text-light me-5"><i class="fa-solid fa-phone"></i> 011 4600 2100</li>
                         <li class="text-light"><i class="fa-solid fa-clock"></i> Mon-Sat 9am - 7pm</li>
                         <i class="fa-solid fa-clock"></i>
@@ -58,14 +98,13 @@
 
 
     <div class="container">
-        <div class="row mt-3 mx-5">
-            <div class="col-12 d-flex justify-content-between">
-                <div class="float-start pt-2">
+        <div class="row mt-3 mx-3">
+            <div class="col-12 d-flex justify-content-between topimg">
+                <div class="col-lg-6 col-md-6 col-sm-12  float-start pt-2">
                     <img src="images/logo.png" alt="">
                 </div>
-                <div class="float-end pt-2">
-                    <img src="images/best-design.jpg" alt="">
-
+                <div id="img2" class="col-lg-6 col-md-6 col-sm-12 col-xs-12 topimg float-end pt-2">
+                    <img src="images/best-design.jpg" class="w-80 mx-2" alt="">
                 </div>
             </div>
         </div>
@@ -93,7 +132,7 @@
     <section id="bannersection">
         <div class="container mt-2">
             <div class="row">
-                <div class="col-5 my-5 order-sm-first order-md-first">
+                <div class="col-lg-5 col-md-5 col-sm-12 my-5 order-sm-first order-md-first">
                     <h2 class="text-light"><i>Designed in Italy</i></h2>
                     <h2 class="mb-5 text-light"><i>Built in India</i></h2>
                     <hr class="bg-danger mb-5" style="height: 8px;">
@@ -106,14 +145,14 @@
                     <h3 class="text-light">Get Quote</h3>
                     <h3 class="text-light">Contact Us</h3>
                 </div>
-                <div class="col-7 align-self-center">
+                <div class="col-lg-7 col-md-7 col-sm-12 align-self-center">
                     <div class="row bg-light ps-3 pe-3 pt-3 pb-3">
-                        <div class="col-lg-5 col-md-12">
-                            <img src="images/form-img.jpg" class="mt-1 mb-1" alt="" id="img-bann">
+                        <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+                            <img src="images/form-img.jpg" class="mt-1 mb-1 w-100" alt="" id="img-bann">
                         </div>
                         <div class="col-lg-7 col-md-12 col-sm-12 align-self-center">
                             <form>
-                                <h5>Get the Best Designed ​Kitchen & Wardrobe</h5>
+                                <h5 class="mt-3">Get the Best Designed ​Kitchen & Wardrobe</h5>
                                 <div class="mb-3 pt-3 w-100">
                                     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your Email Address">
                                 </div>
@@ -148,6 +187,62 @@
         </div>
     </section>
 
+    <!-- <section id="bannersection">
+        <div class="container  mt-2">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12 my-5">
+                    <h2 class="text-light"><i>Designed in Italy</i></h2>
+                    <h2 class="mb-5 text-light"><i>Built in India</i></h2>
+                    <hr class="bg-danger mb-5" style="height: 8px;">
+                    <h3 class="text-light">What we offer</h3>
+                    <h3 class="text-light">Creo Vs Other Brands</h3>
+                    <h3 class="text-light">Why choose CREO</h3>
+                    <h3 class="text-light">Book a Designer</h3>
+                    <h3 class="text-light">Client Testimonials</h3>
+                    <h3 class="text-light">Our Projects</h3>
+                    <h3 class="text-light">Get Quote</h3>
+                    <h3 class="text-light">Contact Us</h3>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 align-self-center">
+                    <div class="row bg-light ps-3 pe-3 pt-3 pb-3">
+                        <div class="form">
+                            <img src="images/ClassicKitchen.jpg" class="mt-1 mb-1 w-100" style="height: 300px;" alt="">
+                        </div>
+                        <div class="align-self-center">
+                            <form>
+                                <h5 class="text-center fs-2 mt-2">MEET A DESIGNER</h5>
+                                <div class="mb-3 pt-3">
+                                    <input type="email" class="form-control rounded-pill" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your Email Address">
+                                </div>
+                                <div class="mb-3">
+                                    <input type="text" class="form-control rounded-pill" id="exampleInputName" placeholder="Full Name">
+                                </div>
+                                <div class="mb-3">
+                                    <input type="text" class="form-control rounded-pill" id="exampleInputName" placeholder="Mobile Number">
+                                </div>
+                                <div class="mb-3">
+                                    <input type="text" class="form-control rounded-pill" id="exampleInputAddress" placeholder="Address">
+                                </div>
+                                <select class="form-select form-select-lg mb-3 rounded-pill" aria-label="Large select example">
+                                    <option selected>Requirement</option>
+                                    <option value="1">Kitchen</option>
+                                    <option value="2">Wardrobes</option>
+                                    <option value="3">Furniture</option>
+                                </select>                                
+                                <div class="text-center gap-2">
+
+                                    <button type="submit" class="btn btn-danger btn-lg rounded-pill">Submit</button>
+                                </div>
+                                <h6 class="mt-1 text-center fw-lighter">By submitting this form, you agree to the privacy policy</h6>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> -->
+
 
     <section>
         <div class="container">
@@ -173,7 +268,7 @@
                                 <div class="card">
                                     <img src="images/ClassicKitchen.jpg" class="card-img-top" alt="...">
                                     <div class="card-body">
-                                        <p class="card-text">Classic Kitchen</p>
+                                        <p class="card-text text-center">Classic Kitchen</p>
                                     </div>
                                 </div>
                             </div>
@@ -193,7 +288,7 @@
                                 <div class="card">
                                     <img src="images/ItalianFurniture.jpg" class="card-img-top" alt="...">
                                     <div class="card-body">
-                                        <p class="card-text">Italian Furniture</p>
+                                        <p class="card-text text-center">Italian Furniture</p>
                                     </div>
                                 </div>
                             </div>
@@ -205,7 +300,7 @@
                                 <div class="card">
                                     <img src="images/ClassicWardrobe.jpg" class="card-img-top" alt="...">
                                     <div class="card-body">
-                                        <p class="card-text">Classic Wardrobe</p>
+                                        <p class="card-text text-center">Classic Wardrobe</p>
                                     </div>
                                 </div>
                             </div>
@@ -215,7 +310,7 @@
                                 <div class="card">
                                     <img src="images/ModernKitchen2.jpg" class="card-img-top" alt="...">
                                     <div class="card-body">
-                                        <p class="card-text">Modern Kitchen</p>
+                                        <p class="card-text text-center">Modern Kitchen</p>
                                     </div>
                                 </div>
                             </div>
@@ -225,7 +320,7 @@
                                 <div class="card">
                                     <img src="images/HomeInteriors.jpg" class="card-img-top" alt="...">
                                     <div class="card-body">
-                                        <p class="card-text">Home Interiors</p>
+                                        <p class="card-text text-center">Home Interiors</p>
                                     </div>
                                 </div>
                             </div>
@@ -471,7 +566,7 @@
     <section id="bannersection">
         <div class="container  mt-2">
             <div class="row">
-                <div class="col-lg-6 col-md-6 col-sm-12 my-5">
+                <div class="col-lg-6 col-md-6 col-sm-12 my-5 bannersection2">
                     <h2 class="text-light"><i>Designed in Italy</i></h2>
                     <h2 class="mb-5 text-light"><i>Built in India</i></h2>
                     <hr class="bg-danger mb-5" style="height: 8px;">
@@ -523,6 +618,145 @@
             </div>
         </div>
     </section>
+
+
+    <div class="container-fluid my-5">
+        <div class="row justify-content-center" style="background-color: papayawhip;">
+            <div class="col-10 mx-5 my-5 mx-5 py-5 bg-light">
+                <h4 class="mt-3 mb-4 mx-5 px-5 fw-light justify-content-center">OUR FINISHED PROJECTS</h4>
+                <p class="fs-2 mx-5 px-5 fw-light my-0 pb-0">Meet our esteemed clients who have adorned</p>
+                <p class="fs-2 mx-5 px-5 fw-light"> ​their spaces with Creo kitchens and wardrobes</p>
+                <hr class="hline text-danger mx-5 px-5 mb-5">
+                <div class="row studiogallery justify-content-center g-0 mb-1 mt-5 my-5">
+                    <div class="col-sm-12 col-md-2 col-lg-2">
+                        <div class="box-card px-3 py-4">
+                            <div class="">
+                                <img src="images/ClassicKitchen.jpg" class="card-img-top w-100 rounded-4" alt="...">
+                                
+                                <div class="">
+                                    <p class="card-text text-center">Leela Bar</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-2 col-lg-2">
+                        <div class="box-card px-3 py-4">
+                            <div class="">
+                                <img src="images/ClassicKitchen.jpg" class="card-img-top w-100 rounded-4" alt="...">
+                                
+                                <div class="">
+                                    <p class="card-text text-center">Leela Bar</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-2 col-lg-2">
+                        <div class="box-card px-3 py-4">
+                            <div class="">
+                                <img src="images/ClassicKitchen.jpg" class="card-img-top w-100 rounded-4" alt="...">
+                                
+                                <div class="">
+                                    <p class="card-text text-center">Leela Bar</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-2 col-lg-2">
+                        <div class="box-card px-3 py-4">
+                            <div class="">
+                                <img src="images/ClassicKitchen.jpg" class="card-img-top w-100 rounded-4" alt="...">
+                                
+                                <div class="">
+                                    <p class="card-text text-center">Leela Bar</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-2 col-lg-2">
+                        <div class="box-car px-3 py-4">
+                            <div class="">
+                                <img src="images/ClassicKitchen.jpg" class="card-img-top w-100 rounded-4" alt="...">
+                                
+                                <div class="">
+                                    <p class="card-text text-center">Leela Bar</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row studiogallery justify-content-center g-0">
+                <div class="col-sm-12 col-md-2 col-lg-2">
+                        <div class="box-car px-3 py-4">
+                            <div class="">
+                                <img src="images/ClassicKitchen.jpg" class="card-img-top w-100 rounded-4" alt="...">
+                                
+                                <div class="">
+                                    <p class="card-text text-center">Leela Bar</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-2 col-lg-2">
+                        <div class="box-car px-3 py-4">
+                            <div class="">
+                                <img src="images/ClassicKitchen.jpg" class="card-img-top w-100 rounded-4" alt="...">
+                                
+                                <div class="">
+                                    <p class="card-text text-center">Leela Bar</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-2 col-lg-2">
+                        <div class="box-car px-3 py-4">
+                            <div class="">
+                                <img src="images/ClassicKitchen.jpg" class="card-img-top w-100 rounded-4" alt="...">
+                                
+                                <div class="">
+                                    <p class="card-text text-center">Leela Bar</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-2 col-lg-2">
+                        <div class="box-car px-3 py-4">
+                            <div class="">
+                                <img src="images/ClassicKitchen.jpg" class="card-img-top w-100 rounded-4" alt="...">
+                                
+                                <div class="">
+                                    <p class="card-text text-center">Leela Bar</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container-fluid bg-dark">
+        <div class="row justify-content-center">
+            <div class="col-10 section1 d-flex">
+                <div class="col-lg-6 col-md-12 col-sm-12">
+                 <img src="images/ClassicWardrobe.jpg" alt="">
+                </div>
+                <div class="col-lg-6 col-md-12 col-sm-12 mt-5 mx-5 justify-content-center align-self-center">
+                  <h2 class="text-light">Get free quote</h2>
+                  <hr class="bg-danger mb-5 text-danger" style="height: 8px;">
+                  <h2 class="text-light">☑ Price you desire,</h2>
+                  <h2 class="text-danger">☑ Quality you deserve,</h2>
+                  <h2 class="text-light">☑ Design you dream of</h2>
+                  <button class="btn btn-light text-center text-dark my-5">Contact today</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container-fluid">
+
+    </div>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
